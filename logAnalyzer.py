89b79d52e7c1e0bd2b864bc70ce1977e3ea05726
Cesar Del Solar <delsolar@gmail.com>
2012-04-21 18:41:01 -0700

    a bit more processing

--- logAnalyzer.py
+++ logAnalyzer.py
@@ -1,7 +1,7 @@
 from xml.etree.cElementTree import ElementTree
 import sys
 from optparse import OptionParser
-
+import json
 usage = "usage: %prog logfile"
 
 parser = OptionParser(usage=usage)
@@ -19,22 +19,50 @@ iterations = tree.findall('iteration')
 
 plyData = {}
 
+def getKey(action, move):
+    key = action
+    if action == "pass":
+        pass
+    elif action == "exchange":
+        key += ':' + move.attrib['tiles']
+    elif action == "place":
+        key += ':' + move.attrib['tiles'] + ':' + move.attrib['position'] + ':' + move.attrib['score']
+    return key
+
 for iteration in iterations:
     pas = iteration.findall('playahead')
     # each of playahead is one of the different moves we're simming/comparing
     for pa in pas:
         plies = pa.findall('ply')
-        for ply in plies:
+        zeroply = plies[0]
+        assert(plies[0].attrib['index']=="0")
+        move = plies[0].find('move')
+        action = move.attrib['action']
+        key = getKey(action, move)
+
+        thisData = []
+
+        for ply in plies[1:]:
             # even plies (starting with 0) are us, odd plies are opponent
             index = ply.attrib['index']
             move = ply.find('move')
             action = move.attrib['action']
-            tiles = move.attrib['tiles']
-            position = move.attrib['position']
-            score = move.attrib['score']
+            inKey = getKey(action, move)
 
-            print index, action, tiles, position, score
+            rack = ply.find('rack')
+            pc = ply.find('pc')
+            if pc is not None:
+                leaveValue = pc.attrib['value']
+            else:
+                leaveValue = None
+            tiles = rack.attrib['tiles']
+            thisData.append( {index: tiles + ':' + inKey + (':' + leaveValue if leaveValue else '')} )
 
+        if key not in plyData:
+            plyData[key] = [thisData]
+        else:
+            plyData[key].append(thisData)
 
+print json.dumps(plyData, indent=2)
 
 
